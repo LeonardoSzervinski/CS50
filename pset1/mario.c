53d2ab8bd3f9eac86ed0b27885c741f441bef112
szervinski <leonardo.szervinski@hotmail.com>
2023-11-02 05:00:56 -0300

    mario.c
    Added left to right pyramid

--- pset1/mario.c
+++ pset1/mario.c
@@ -5,11 +5,26 @@ int main(void)
 {
     int height;
 
-    // Verification from the height 
+    // Verification from the height
     do
     {
-        height = get_string("Write a value this (1 one 8): ");
+        height = get_int("Write a value this (1 one 8): ");
     } while (height < 1 || height > 8);
 
-    
+    // Building Pyramid
+    for (int line = 0; line < height; line++)
+    {
+        // Building Blanks
+        for (int column = height; column > line; column--)
+        {
+            printf(" ");
+        }
+
+        // Addition hashes from the lines and column
+        for (int hashes = -1; hashes < line; hashes++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
